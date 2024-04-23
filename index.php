efaf4ba8fec4822ee25458056759b060f33a927c
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-04-23 12:08:06 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+echo"Hello World";
+echo"<br>";
+$var1 =10;
+$var2=15;
+
+echo "The value after calculation is";
+echo $var1 + $var2;
+
+echo"<br>";
+
+//arithmetic op
+
+echo"Using Multiplication Rule : ";
+echo $var1 * $var2;
+
+//assignment op
+
+echo"<br>";
+$newvar=$var1;
+echo"Value of newvar:";
+echo $newvar+1;
+
+
+
+
+?>
