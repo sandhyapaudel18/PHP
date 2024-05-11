3e1c195a9ffb0f3cef7f1f2044e13ea5cce3499e
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-05-11 06:36:23 +0545

    changes

---
+++ radiobtn.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RadioButtons</title>
+</head>
+<body>
+    <form action="./radiobtn.php" method="post">
+        <input type="radiobutton" value="Visa" name="cc">VISA<br><br>
+        <input type="radiobutton" value="Visa" name="cc" >MASTER<br><br>
+        <input type="radiobutton" value="Visa" name="cc" >AMERICAN<br><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+<
