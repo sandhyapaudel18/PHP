c94e64952a8b1d5ab46824451d849a8b897ca5ee
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-05-13 10:27:09 +0545

    changes

---
+++ exceptionhandling.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./exceptionhandling.php" method="post">
+        <label for="">X:</label>
+        <input type="text" name="x"/><br><br>
+
+        <label for="">Y:</label>
+        <input type="text" name="y"/><br><br>
+
+        <input type="submit" value="submit">
+
+    </form>
+</body>
+</html>
+<?php 
+$x=$_POST['x'];
+$y=$_POST['y'];
+
+try{
+    checknum($y);
+    echo"NUMBER LESS THAN 0";
+}
+catch(Exception $e ){
+
+
+    
+}
+
+?>
