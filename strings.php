7c0bd4cf874d67de5d63a8b8c81fec8f3ba85767
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2024-05-13 08:55:19 +0545

    changes in strings

--- strings.php
+++ strings.php
@@ -13,12 +13,35 @@ $phone=9847158150;
 // $username=str_pad($username,20,"1");
 
 //just replace the num or char present in the strings:
-$phone=str_replace("9","0",$phone);
-echo $phone;
+// $phone=str_replace("9","0",$phone);
+
+//rev obsly backwards
+// $phone=strrev($phone);
+// echo $phone;
+//shuffle means j paye tahi order ma huner
+// $username=str_shuffle($username);
+
+// //while compare 0 =same 1= not same
+// $username=strcmp($username,"");
+
+// echo $username;
 
 
+// $count=strlen($username);
+// echo $count;
 
-echo $username;
+// //position
+// $count1=strpos($username,"a");
+// echo $count1;
 
+
+// substring=creation of new string from the string itself
+// $main=substr($username,0,8);
+// echo $main;
+
+// $fllname=explode("",$username);
+// foreach($fllame as  $name){
+//     echo $name."<br>";
+// }
 
 ?>
